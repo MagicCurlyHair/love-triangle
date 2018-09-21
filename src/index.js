4e797e89a1a299388601a9890ca29a532e203c42
MagicCurlyHair <g.shokanov@gmail.com>
2018-09-21 10:06:57 +0300

    function getLoveTrianglesCount counts triangles correctly

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let a;
+  let b;
+  let c;
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++){
+    // increments/decrements by 1 because provided array is 1-indexed
+    a = preferences[i];
+    b = preferences[a - 1];
+    c = preferences[b - 1];
+    if (c == i + 1 && a != b && b != c && c != a){
+      count++;
+    }
+  }
+  // goes through every love triangle three times, so should divide by 3 to get the result
+  return count / 3;
 };
